181fac89ee8ddd9d6cd30bedcc0ba2c0941bef6c
ThomasRichard <thorichard@icloud.com>
2025-03-04 15:35:20 +0100

    Bdd contient 14k echantillons, améliorations script

---
+++ Analyse_de_Données/Projet_AD/All_Fragrances_Dataset/Combined_ds.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df_all = pd.read_csv('All_Fragrantica_dataset.csv')
+df = pd.read_csv('Fragrantica_dataset.csv')
+print(df_all.shape)
+print(df.shape)
+combined_df = pd.concat([df_all, df], axis=0)
+combined_df = combined_df.drop_duplicates(subset=['nom_parfum', 'marque', 'launch_year', 'url'], keep='first')
+print(combined_df.shape)
+combined_df.to_csv('All_Fragrantica_Dataset.csv', index=False)
+
+
