097bf5af7ba819867a068e1164b1be2bb3f3b267
ThomasRichard <thorichard@icloud.com>
2025-04-04 10:32:41 +0200

    Exploration data

--- Analyse_de_Données/Projet_AD/Scraping_Fragrantica/Combined_ds.py
+++ Analyse_de_Données/Projet_AD/Scraping_Fragrantica/Combined_ds.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 df1= pd.read_csv('Fragrantica_dataset.csv')
-df2 = pd.read_csv('Fragrantica_dataset2.csv')
+df2 = pd.read_csv('Ajout_Fragrantica_dataset.csv')
 print(df1.shape)
 print(df2.shape)
 combined_df = pd.concat([df1, df2], axis=0)
 combined_df = combined_df.drop_duplicates(subset=['url'], keep='first')
 print(combined_df.shape)
-combined_df.to_csv('All_Fragrantica_Dataset.csv', index=False)
+combined_df.to_csv('Fragrantica_Dataset.csv', index=False)
 
 
